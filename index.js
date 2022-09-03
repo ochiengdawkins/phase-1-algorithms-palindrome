21f114e08c1ee15f73dcfd35fd3e74a2e6436c18
DawkinsJonathanOchieng <dawkinsjonathan871@gmail.com>
2022-09-03 08:46:21 +0300

    commit tree 6bf09a71826d253293671d07da8697ec555eac77

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word.split("").reverse().join("");
+
+}
+function reverse(word) {
+  // 'abc' => 'cba'
+  // const wordArray = word.split("");
+  // const reversedWordArray = wordArray.reverse();
+  // const reversedWord = reversedWordArray.join("");
+  // return reversedWord;
+  return word.split("").reverse().join("");
 }
 
+function isPalindrome(word) {
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
+}
 /* 
   Add your pseudocode here
 */
+//return word.split("").reverse().join("");
 
 /*
   Add written explanation of your solution here
 */
+// 'Joseph ' will reverse to 'hpesoJ'
+// const word_array = word.split(""); // using .split to remove blank spaces /spliting a string into an array of substrings
+// const reverse = wordArray.reverse(); // using .reverse to reverse the word
+// const reversed_word = reversedWordArray.join("");  //returning our array as a string using the join method
+// return reversed_word;
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
